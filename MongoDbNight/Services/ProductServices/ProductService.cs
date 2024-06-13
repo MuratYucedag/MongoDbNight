7789f71599592cd0088ce79286291510d7351d09
MuratYucedag <yucedagmurat23@gmail.com>
2024-06-13 20:42:30 +0300

    ...

---
+++ MongoDbNight/Services/ProductServices/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MongoDbNight.Dtos.ProductDtos;
+using MongoDbNight.Entities;
+using MongoDbNight.Settings;
+
+namespace MongoDbNight.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+        private readonly IMapper _mapper;
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettins)
+        {
+            var client = new MongoClient(_databaseSettins.ConnectionString);
+            var database = client.GetDatabase(_databaseSettins.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettins.ProductCollectionName);
+            _mapper = mapper;
+        }
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdProductDto> GetByIdProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
